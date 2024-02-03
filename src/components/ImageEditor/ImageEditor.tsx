41d86d69417cfbb271f41aa48ddccb19d315cd7f
Mohammad Mohitul Islam <mohitul449@gmail.com>
2024-02-03 22:18:48 +0600

    filter option is working with value changes not in image

--- src/components/ImageEditor/ImageEditor.tsx
+++ src/components/ImageEditor/ImageEditor.tsx
@@ -1,4 +1,52 @@
+import { useState } from 'react';
+
 const ImageEditor = () => {
+  const [brightness, setBrightness] = useState(100);
+  const [saturation, setSaturation] = useState(100);
+  const [inversion, setInversion] = useState(0);
+  const [grayscale, setGrayscale] = useState(0);
+  const [currentFilter, setCurrentFilter] = useState<string>('Brightness');
+  // const [rotate, setRotate] = useState(0);
+  // const [flipHorizontal, setFlipHorizontal] = useState(1);
+  // const [flipVertical, setFlipVertical] = useState(1);
+  const [imageFile, setImageFile] = useState<File | null>(null);
+
+  const loadImage = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file: File | null = e.target.files ? e.target.files[0] : null;
+    if (!file) return;
+    setImageFile(file);
+  };
+
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  //@ts-ignore
+  const handleChangeControlValue = (controlName: string, value) => {
+    switch (controlName) {
+      case 'Brightness':
+        setBrightness(value);
+        break;
+      case 'Saturation':
+        setSaturation(value);
+        break;
+      case 'Inversion':
+        setInversion(value);
+        break;
+      case 'Grayscale':
+        setGrayscale(value);
+        break;
+      default:
+        break;
+    }
+    setCurrentFilter(controlName);
+  };
+
+  const handleResetFilters = () => {
+    setBrightness(100);
+    setSaturation(100);
+    setInversion(0);
+    setGrayscale(0);
+    setCurrentFilter('Brightness');
+  };
+
   return (
     <div className='grid grid-cols-1 tablet:grid-cols-2 gap-5 p-5'>
       {/* left */}
@@ -9,16 +57,48 @@ const ImageEditor = () => {
           <div className=''>
             <label className='text-[2rem] font-semibold'>Filters</label>
             <div className='grid grid-cols-1 tablet:grid-cols-2 gap-5 py-3'>
-              <button id='brightness' className='control-options'>
+              <button
+                id='brightness'
+                className={`${
+                  currentFilter === 'Brightness'
+                    ? 'control-options bg-[#ff6347]'
+                    : 'control-options'
+                }`}
+                onClick={() => setCurrentFilter('Brightness')}
+              >
                 Brightness
               </button>
-              <button id='saturation' className='control-options'>
+              <button
+                id='saturation'
+                className={`${
+                  currentFilter === 'Saturation'
+                    ? 'control-options bg-[#ff6347]'
+                    : 'control-options'
+                }`}
+                onClick={() => setCurrentFilter('Saturation')}
+              >
                 Saturation
               </button>
-              <button id='inversion' className='control-options'>
+              <button
+                id='inversion'
+                className={`${
+                  currentFilter === 'Inversion'
+                    ? 'control-options bg-[#ff6347]'
+                    : 'control-options'
+                }`}
+                onClick={() => setCurrentFilter('Inversion')}
+              >
                 Inversion
               </button>
-              <button id='grayscale' className='control-options'>
+              <button
+                id='grayscale'
+                className={`${
+                  currentFilter === 'Grayscale'
+                    ? 'control-options bg-[#ff6347]'
+                    : 'control-options'
+                }`}
+                onClick={() => setCurrentFilter('Grayscale')}
+              >
                 Grayscale
               </button>
             </div>
@@ -27,15 +107,38 @@ const ImageEditor = () => {
           {/* Filter Range Control */}
           <div className='mt-3'>
             <div className='filter-info'>
-              <p className='name'>Brighteness</p>
-              <p className='value'>100%</p>
+              <p className='name'>{currentFilter}</p>
+              <p className='value'>
+                {currentFilter === 'Brightness'
+                  ? brightness
+                  : currentFilter === 'Saturation'
+                  ? saturation
+                  : currentFilter === 'Inversion'
+                  ? inversion
+                  : grayscale}
+              </p>
             </div>
             <input
               className='slider-range'
               type='range'
-              value='100'
+              value={
+                currentFilter === 'Brightness'
+                  ? brightness
+                  : currentFilter === 'Saturation'
+                  ? saturation
+                  : currentFilter === 'Inversion'
+                  ? inversion
+                  : grayscale
+              }
               min='0'
-              max='200'
+              max={`${
+                currentFilter === 'Inversion' || currentFilter === 'Grayscale'
+                  ? '100'
+                  : '200'
+              }`}
+              onChange={(e) =>
+                handleChangeControlValue(currentFilter, e.target.value)
+              }
             />
           </div>
 
@@ -121,18 +224,28 @@ const ImageEditor = () => {
 
         {/* Reset Functions */}
         <div>
-          <button className='btn-primary mt-4'>{'Reset All'}</button>
+          <button className='btn-primary mt-4' onClick={handleResetFilters}>
+            {'Reset All'}
+          </button>
         </div>
       </div>
 
       {/* Right */}
       <div>
         <div className='preview-img'>
-          <img src='/common/image-placeholder.svg' alt='preview-img' />
+          <img
+            src={`${
+              imageFile
+                ? URL.createObjectURL(imageFile)
+                : '/common/image-placeholder.svg'
+            }`}
+            alt='preview-img'
+          />
         </div>
         <div className='mt-4 flex items-center justify-between gap-3'>
-          <button className='btn-primary '>{'Choose Image'}</button>
-          <button className='btn-primary '>{'Save Image'}</button>
+          <input className='' type='file' onChange={loadImage} />
+
+          {imageFile && <button className='btn-primary'>{'Save Image'}</button>}
         </div>
       </div>
     </div>
