f68e84f21acc9790e7104d97ff48df44c505a06d
Mohammad Mohitul Islam <mohitul449@gmail.com>
2024-02-04 08:40:21 +0600

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ultimate-image-editor/',
+  // base: '/ultimate-image-editor/',
+  server: {
+    port: 3001,
+  },
   plugins: [react()],
 });
